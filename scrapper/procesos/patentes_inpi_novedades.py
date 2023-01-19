aac0f9f1be5d04cb906f51aca058772d2ec4ca50
Patricio <pmoracho@gmail.com>
2023-01-19 20:19:57 -0300

    Finalmente salva y renombra descargas en modo headless

--- scrapper/procesos/patentes_inpi_novedades.py
+++ scrapper/procesos/patentes_inpi_novedades.py
@@ -3,16 +3,10 @@ patentes_inpi_novedades
 """
 import os
 import time
-import traceback
+import shutil
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from progressbar import ProgressBar
-from progressbar import FormatLabel
-from progressbar import Percentage
-from progressbar import Bar
-from progressbar import RotatingMarker
-from progressbar import ETA
 
 def patentes_inpi_novedades(driver,
                             parametros,
@@ -27,6 +21,40 @@ def patentes_inpi_novedades(driver,
 
     inputfile: Archivo tipo csv con numero de solicitud y tipo de documento
     """
+
+    def get_last_downloaded_file_path(dummy_dir):
+        """ Return the last modified -in this case last downloaded- file path.
+
+            This function is going to loop as long as the directory is empty.
+        """
+        while not os.listdir(dummy_dir):
+            time.sleep(1)
+        return max([os.path.join(dummy_dir, f) for f in os.listdir(dummy_dir)], key=os.path.getctime)
+
+    # method to get the downloaded file name
+    def getDownLoadedFileName(waitTime):
+        driver.execute_script("window.open()")
+        # switch to new tab
+        driver.switch_to.window(driver.window_handles[-1])
+        # navigate to chrome downloads
+        driver.get('chrome://downloads')
+        # define the endTime
+        endTime = time.time()+waitTime
+        while True:
+            try:
+                # get downloaded percentage
+                downloadPercentage = driver.execute_script(
+                    "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
+                # check if downloadPercentage is 100 (otherwise the script will keep waiting)
+                if downloadPercentage == 100:
+                    # return the file name once the download is completed
+                    return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+            except:
+                pass
+            time.sleep(1)
+            if time.time() > endTime:
+                break
+
 
     def get_downloaded_filename(wait_time):
 
@@ -92,12 +120,18 @@ def patentes_inpi_novedades(driver,
                     EC.visibility_of_element_located((By.XPATH, descarga_xpath))
                 )
 
+                #file = btn.get_property("href").split("=")[2]
                 btn.click()
                 log.info("click sobre el botón de descarga")
 
-                latest_downloaded_filename = get_downloaded_filename(60)
+                latest_downloaded_filename = get_last_downloaded_file_path(outputpath)
+                _, file_extension = os.path.splitext(latest_downloaded_filename)
+                nfile = len(files) + 1
                 if latest_downloaded_filename:
-                    files.append(os.path.join(outputpath, latest_downloaded_filename))
+                    new_file = os.path.join(outputpath,
+                                            f"{solicitud_a_buscar}-{tipo_doc}-{nfile}{file_extension}")
+                    shutil.move(latest_downloaded_filename, new_file)
+                    files.append(os.path.join(outputpath, new_file))
                 else:
                     files = []
 
--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -6,6 +6,21 @@ from selenium.webdriver.chrome.options import Options
 
 from scrapper.procesos.patentes_inpi_novedades import patentes_inpi_novedades
 
+def enable_download_in_headless_chrome(driver, download_dir):
+    """
+    there is currently a "feature" in chrome where
+    headless does not allow file download: https://bugs.chromium.org/p/chromium/issues/detail?id=696481
+    This method is a hacky work-around until the official chromedriver support for this.
+    Requires chrome version 62.0.3196.0 or above.
+    """
+
+    # add missing support for chrome "send_command"  to selenium webdriver
+    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+
+    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+    command_result = driver.execute("send_command", params)
+
+
 def get_chrome_driver(download_folder, show=False):
     """Configura y retorna el driver chrome
     """
@@ -21,14 +36,21 @@ def get_chrome_driver(download_folder, show=False):
     chrome_options.add_argument("--window-size=1920,1080")
 
     if not show:
-        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless=chrome')
 
     chrome_options.add_experimental_option('prefs',
                                             {'download.default_directory' : download_folder,
                                             'service_log_path' : download_folder,
-                                            "directory_upgrade": True})
+                                            "download.prompt_for_download": False,
+                                            "directory_upgrade": True,
+                                            'safebrowsing.enabled': False,
+                                            'safebrowsing.disable_download_protection': True
+                                            })
 
-    return webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
+    #enable_download_in_headless_chrome(driver, download_folder)
+
+    return driver
 
 
 def scrap(proceso,
