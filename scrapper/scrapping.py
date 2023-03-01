555c087255d851875a32acdd59e78c0c4bc1a9a0
Patricio <pmoracho@gmail.com>
2023-03-01 18:03:52 -0300

    fix: opciones de webdriver

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -14,36 +14,48 @@ def get_chrome_driver(download_folder, show=False):
     """Configura y retorna el driver chrome
     """
 
+    # chrome_options = Options()
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument("--disable-popup-blocking")
+    # # pylint: disable=line-too-long
+    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+    # chrome_options.add_argument('--start-maximized')
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument('--disable-infobars')
+    # chrome_options.add_argument("--window-size=1920,1080")
+    # chrome_options.add_argument(f"--download-directory={download_folder}")
+    # if not show:
+    #     chrome_options.add_argument("--headless=new")
+
+
+
+    # chrome_options.add_experimental_option('prefs',
+    #                                         {'download.default_directory' : download_folder,
+    #                                         "download.prompt_for_download": False,
+    #                                         "download.directory_upgrade": True,
+    #                                         'service_log_path' : download_folder,
+    #                                         'safebrowsing.enabled': True,
+    #                                         'safebrowsing.disable_download_protection': True
+    #                                         })
+
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument("--disable-popup-blocking")
-    # pylint: disable=line-too-long
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-    chrome_options.add_argument('--start-maximized')
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument('--disable-infobars')
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument(f"--download-directory={download_folder}")
     if not show:
-        chrome_options.add_argument('--headless=chrome')
+        chrome_options.add_argument("--headless=new")
 
-
-
-    chrome_options.add_experimental_option('prefs',
-                                            {'download.default_directory' : download_folder,
-                                            "download.prompt_for_download": False,
-                                            "download.directory_upgrade": True,
-                                            'service_log_path' : download_folder,
-                                            'safebrowsing.enabled': True,
-                                            'safebrowsing.disable_download_protection': True
-                                            })
+    chrome_options.add_argument(f"--download-directory={download_folder}")
+    chrome_options.add_experimental_option("prefs", {
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "download.default_directory": download_folder,
+        "safebrowsing.enabled": True,
+        'safebrowsing.disable_download_protection': True
+    })
 
     driver = webdriver.Chrome(options=chrome_options)
 
     return driver
-
 
 
 def scrap(proceso,
