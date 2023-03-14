598cd5562aa52ef110c4d41dce6c36f705f99f27
Patricio Moracho <pmoracho@gmail.com>
2023-03-14 11:41:17 -0300

    fix: removemos mensaje de log

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -9,36 +9,11 @@ from selenium.webdriver.chrome.options import Options
 from scrapper.procesos.patentes_inpi_novedades import patentes_inpi_novedades
 from scrapper.procesos.zonaprop import zonaprop
 from scrapper.procesos.dummy import dummy_download_file
+from scrapper.procesos.inpi_novedades import inpi_novedades
 
 def get_chrome_driver(download_folder, show=False):
     """Configura y retorna el driver chrome
     """
-
-    # chrome_options = Options()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument("--disable-popup-blocking")
-    # # pylint: disable=line-too-long
-    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-    # chrome_options.add_argument('--start-maximized')
-    # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument('--disable-infobars')
-    # chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument(f"--download-directory={download_folder}")
-    # if not show:
-    #     chrome_options.add_argument("--headless=new")
-
-
-
-    # chrome_options.add_experimental_option('prefs',
-    #                                         {'download.default_directory' : download_folder,
-    #                                         "download.prompt_for_download": False,
-    #                                         "download.directory_upgrade": True,
-    #                                         'service_log_path' : download_folder,
-    #                                         'safebrowsing.enabled': True,
-    #                                         'safebrowsing.disable_download_protection': True
-    #                                         })
 
     chrome_options = Options()
     if not show:
@@ -54,8 +29,6 @@ def get_chrome_driver(download_folder, show=False):
     })
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=chrome_options)
-    log.info("Configuración del driver exitosa")
-
     return driver
 
 
