1017fd6cff3cc4a0907143df819e028610162935
Patricio Moracho <pmoracho@gmail.com>
2023-03-14 11:43:18 -0300

    feat: log ahora requiere path completo al archivo

--- scrapper/log.py
+++ scrapper/log.py
@@ -1,11 +1,14 @@
+"""Manejo de Log
+"""
 import logging
-import os
 
 class Log:
+    """Clase para el manejo de una auditoría ("Log") del proceso
+    """
 
-    def __init__(self, outputpath, loglevel, quiet = True):
+    def __init__(self, filename, loglevel, quiet = True):
 
-        logging.basicConfig(filename=os.path.join(outputpath, 'scrapper.log'),
+        logging.basicConfig(filename=filename,
                             level=getattr(logging, loglevel.upper(), None),
                             format='%(asctime)s|%(levelname)s|%(message)s',
                             datefmt='%Y/%m/%d %I:%M:%S',
@@ -14,25 +17,25 @@ class Log:
 
 
     def info(self, msg):
-
+        """Log informativo"""
         if not self.quiet:
             print(msg)
 
         logging.info(msg.replace("|", " "))
 
     def debug(self, msg):
-
+        """Log nivel debug"""
         logging.debug(msg.replace("|", " "))
 
     def exception(self, msg):
-
+        """Log nivel excption"""
         if not self.quiet:
             print(f"!!!Se ha producido una excepción {msg}")
 
         logging.exception(msg.replace("|", " "))
 
     def error(self, msg):
-
+        """Log nivel error"""
         if not self.quiet:
             print(f"!!!Se ha producido un error {msg}")
 
